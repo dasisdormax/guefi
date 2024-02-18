aa5398e1daed5bf46061a55c6433ae6f1e690973
Maximilian Wende <dasisdormax@mailbox.org>
2024-02-18 11:20:40 +0100

    Add basic demo UI layout

--- guefi/src/main.rs
+++ guefi/src/main.rs
@@ -1,5 +1,57 @@
 use guefi_lib::add;
+use iced::{futures::FutureExt, widget::{button, checkbox, column, container, scrollable, text, vertical_space}, window::{self, Action}, Command, Element, Length, Sandbox, Settings, Theme};
 
-fn main() {
-    println!("Hello, world, 1+2 is {}.", add(1, 2));
+#[derive(Debug, Clone)]
+enum Message {
+    AddOne,
+    AddText(String),
+    Clear
+}
+
+#[derive(Default)]
+struct MainWindow {
+    messages: Vec<String>
+}
+
+impl Sandbox for MainWindow {
+    type Message = Message;
+
+    fn new() -> Self {
+        MainWindow::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("guEFI")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::Clear => self.messages.clear(),
+            Message::AddOne => {
+                let msg = Message::AddText(format!("{}", self.messages.len() + 1));
+                self.update(msg);
+            }
+            Message::AddText(text) => self.messages.push(text)
+        };
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        let mut cols: Vec<Element<_>> = vec![
+            text("guEFI").into(),
+            checkbox("Check!", false).into(),
+            button("AddOne").on_press(Message::AddOne).into(),
+            button("Clear").on_press(Message::Clear).into()
+        ];
+        let items: Vec<Element<_>> = self.messages.iter().map(|item| text(item).into()).collect();
+        //cols.splice(2..2, items);
+        let item_list = scrollable(
+            column(items).spacing(5).padding(5)
+        ).width(Length::Fill).height(Length::Fill).into();
+        cols.insert(2, item_list);
+        column(cols).spacing(10).padding(10).into()
+    }
+}
+
+fn main() -> iced::Result {
+    MainWindow::run(Settings::default())
 }
